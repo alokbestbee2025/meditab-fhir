15d2165d03f6199b88d1cee9304299414a2ede00
Alok Singh <aloks@Meditab.Local>
2025-08-06 17:33:50 +0530

    new build file 6 August 25

--- .vercel/output/static/_nuxt/CbhfWTjr.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as r,z as t,o as s}from"./Bv4Txu0p.js";const c={};function n(e,a){return s(),r("th",null,[t(e.$slots,"default")])}const _=o(c,[["render",n]]);export{_ as default};
--- .vercel/output/static/_nuxt/XxcZ4Xes.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as r,z as s,o as t}from"./Bv4Txu0p.js";const c={};function n(e,l){return t(),r("ol",null,[s(e.$slots,"default")])}const _=o(c,[["render",n]]);export{_ as default};
