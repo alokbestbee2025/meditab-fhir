0c496dc96ceeb2d72063c9fced336fc964563f70
Alok Singh <aloks@Meditab.Local>
2025-09-19 15:04:56 +0530

    vercel added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { generateRoutes } from './utils/routes'
 export default defineNuxtConfig({
   ssr: false,
     nitro: {
-    preset: 'vercel-static',
+    preset: 'vercel',
     prerender: {
       crawlLinks: true,
       routes: ['/']
